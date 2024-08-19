385a572bda5d3e7cc496a1acf840a85daf801a47
Mark Kittisopikul <markkitt@gmail.com>
2024-08-18 23:44:56 -0400

    Add a Blink-based test to check Mux server

--- test/mux-tests.jl
+++ test/mux-tests.jl
@@ -1,9 +1,17 @@
 using Test
 using WebIO
 using Mux
+using Blink
 
 @testset "Mux sanity" begin
     @test isdefined(WebIO, :webio_serve)
+end
+
+w = Window()
+@testset "Mux + Blink" begin
+    t = WebIO.webio_serve(page("/", req -> dom"div"("hello")), 8006)
+    loadurl(w, "http://localhost:8006")
+    @test !istaskfailed(t.task)
 end
 
 # TODO: real mux tests (possibly using Blink as a headless chrome)
